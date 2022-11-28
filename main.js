589187da699323e999ad960638ffd3aa2723e362
MANOELA Urias <100574391+Uriasmanu@users.noreply.github.com>
2022-11-28 06:14:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//Lista de destinos
+
+var listaDeCidades =["Marilia", "São Paulo", "Lins", "Botucatu"];
+let idadeComprador = 16;
+
+if (idadeComprador >= 18) {
+        console.log(listaDeCidades);
+}
+else{
+    console.log ("Você não tem permissão para viajar sozinho")
+}
+    
+
